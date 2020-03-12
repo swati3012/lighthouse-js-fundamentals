b4cac9f5dc9048986ad76c827214f907d9cc438c
Swati Chauhan <swati3012@gmail.com>
2020-03-12 09:48:37 +0000

    Extreme temperature

--- conditionals.js
+++ conditionals.js
@@ -1,6 +1,4 @@
-const isCitizen = true;
-const age = 26;
-
-if (isCitizen && age > 18) {
-  console.log("You are eligible to vote.");
+const temperature = 50
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
 }
