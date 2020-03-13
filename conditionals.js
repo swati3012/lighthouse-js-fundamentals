02e9f97cdb4117e90e0bfbfc0d5dd336b3f8b1db
Swati Chauhan <swati3012@gmail.com>
2020-03-13 09:56:23 +0000

    JuliaJames

--- conditionals.js
+++ conditionals.js
@@ -1,4 +1,27 @@
-const raining = false
-if (!raining) {
-  console.log("Leave your umbrella at home!");
+var x = 1;
+
+while (x <= 20/* your stop condition goes here */) 
+{
+  if ((x%3 === 0) && (x%5 === 0)) // check divisibility 
+  { 
+      console.log ("JuliaJames");
+      
+  } // print Julia, James, or JuliaJames
+  else if (x % 5 ===0)
+    { 
+        console.log ("James");
+        
+    }
+  else if (x % 3 === 0) 
+    {
+        console.log("Julia");
+        
+    }
+  else
+    {
+        console.log(x);
+        
+    }
+    x = x+1;// increment x
 }
+
